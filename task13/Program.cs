89ae8717a0d60ec2ab8902268c0c3fc5abb084a6
Ekaterina <missathlon08@gmail.com>
2022-09-03 11:31:43 +0300

    add Task 13

---
+++ task13/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Vvedite chislo a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100) Console.Write("Tret`ej tsifry net");
+else Console.Write(Convert.ToString(a)[2]);
